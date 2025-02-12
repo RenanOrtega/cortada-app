c4f8aa629496522acb8c7380bc18bf27234300b4
Renan Johny Ortega <renanortega82@gmail.com>
2025-02-11 23:29:57 -0300

    feat: add splash and onboarding page.

---
+++ lib/presentation/pages/onboarding/onboarding_page.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatefulWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  final List<OnboardingItem> _pages = [
+    OnboardingItem(
+      title: 'Bem vindo ao Cortada',
+      description: 'Toma que toma toma',
+      image: 'assets/images/onboarding.png',
+    ),
+    OnboardingItem(
+      title: 'Crie partidas com seus amigos',
+      description: 'Toma que toma toma',
+      image: 'assets/images/onboarding.png',
+    ),
+    OnboardingItem(
+      title: 'Rankings mensais e  cosméticos maneiros',
+      description: 'Toma que toma toma',
+      image: 'assets/images/onboarding.png',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                controller: _pageController,
+                onPageChanged: (int page) {
+                  setState(() {
+                    _currentPage = page;
+                  });
+                },
+                itemCount: _pages.length,
+                itemBuilder: (context, index) {
+                  return _buildPage(_pages[index]);
+                },
+              ),
+            ),
+            _buildIndicators(),
+            _buildButtons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPage(OnboardingItem item) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            item.image,
+            height: 300,
+          ),
+          const SizedBox(height: 40),
+          Text(
+            item.title,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 20),
+          Text(
+            item.description,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIndicators() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(
+        _pages.length,
+        (index) => Container(
+          margin: const EdgeInsets.symmetric(horizontal: 4),
+          width: 8,
+          height: 8,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: _currentPage == index ? Colors.black : Colors.grey[600],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButtons() {
+    return Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            if (_currentPage > 0)
+              TextButton(
+                onPressed: () {
+                  _pageController.previousPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                },
+                child: const Text(
+                  'Anterior',
+                  style: TextStyle(color: Colors.black),
+                ),
+              )
+            else
+              const SizedBox(width: 80),
+            TextButton(
+              onPressed: () {
+                if (_currentPage < _pages.length - 1) {
+                  _pageController.nextPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                } else {
+                  Navigator.pushReplacementNamed(context, '/home');
+                }
+              },
+              child: Text(
+                _currentPage < _pages.length - 1 ? 'Próximo' : 'Começar',
+                style: const TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class OnboardingItem {
+  final String title;
+  final String description;
+  final String image;
+
+  OnboardingItem({
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+}
---
+++ lib/presentation/pages/splash/splash_page.dart
@@ -0,0 +1,50 @@
+import 'package:cortada_app/presentation/pages/onboarding/onboarding_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToOnboarding();
+  }
+
+  Future<void> _navigateToOnboarding() async {
+    await Future.delayed(const Duration(seconds: 3));
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) => const OnboardingPage(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: 200,
+              height: 200,
+            ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
